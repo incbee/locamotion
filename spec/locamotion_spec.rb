6741202fb2b0f098fb6c5c989deee7ccaddb8454
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2014-12-26 19:07:19 +0100

    Get first spec running

--- spec/locamotion_spec.rb
+++ spec/locamotion_spec.rb
@@ -1,4 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Locamotion" do
+  before do
+    FileUtils.rm_rf('/tmp/rm_project')
+    FileUtils.mkdir_p('/tmp/rm_project/app')
+    content = <<-eos
+    "I am localized!"._
+    "I am also localized."._
+    eos
+    File.open('/tmp/rm_project/app/test1.rb', 'a+') { |f| f.write(content) }
+    File.open('/tmp/rm_project/app/test2.rb', 'a+') { |f| f.write("\"I am to be localized\"._") }
+    @app = Locamotion::App.new 
+  end
+
+  describe "slurp" do
+    it "gets all localizable strings" do
+      expect(@app.parse_rubymotion_app('/tmp/rm_project/app/**/*.rb')).to match_array(["I am localized!", "I am also localized.", "I am to be localized"])
+    end
+  end
 end
