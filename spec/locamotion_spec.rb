57360f7a48d99aaeaa4b34a21355142f2041f1e6
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2014-12-26 19:36:50 +0100

    Add tests for generate

--- spec/locamotion_spec.rb
+++ spec/locamotion_spec.rb
@@ -34,4 +34,18 @@ describe "Locamotion" do
       expect(@app.announce_slurp_results(0)).to eq("No new strings added.")
     end
   end
+
+  describe "generate" do
+    it "announces 3 german strings" do
+      expect(@app.announce_generate_results(3, '/tmp/rm_project/resources/de.lproj/Localizable.strings')).to eq("3 new strings entered into /tmp/rm_project/resources/de.lproj/Localizable.strings")
+    end
+    
+    it "announces 1 german strings" do
+      expect(@app.announce_generate_results(1, '/tmp/rm_project/resources/de.lproj/Localizable.strings')).to eq("1 new string entered into /tmp/rm_project/resources/de.lproj/Localizable.strings")
+    end
+
+    it "announces no german strings" do
+      expect(@app.announce_generate_results(0, '/tmp/rm_project/resources/de.lproj/Localizable.strings')).to eq("No new strings added to /tmp/rm_project/resources/de.lproj/Localizable.strings.")
+    end
+  end
 end
